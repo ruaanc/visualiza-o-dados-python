eb8eb28caf44c5af8d88152ad110de155d098736
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-13 15:57:24 -0300

    Adicionando Legendas

--- grafico.py
+++ grafico.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt 
 
-x = [1, 2]
-y = [2, 3]
+x = [1, 2, 5]
+y = [2, 3, 7]
+
+plt.title("Meu primeiro gráfico com python")
+
+#Eixos
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
 
 plt.plot(x, y)
 plt.show()
